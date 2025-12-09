47cb856885657ef3a1e428cedf46cff86ff208da
Gergana Roshleva <gergana.roshleva@gmail.com>
2025-12-09 16:46:06 +0200

    Removed event models from user model

--- src/modules/user/userIdentity.entity.ts
+++ src/modules/user/userIdentity.entity.ts
@@ -7,13 +7,9 @@ import {
   DataType,
   Unique,
   HasOne,
-  HasMany,
 } from "sequelize-typescript";
 import { v4 as uuidv4 } from "uuid";
 import { UserProfile } from "./userProfile.entity";
-import { Event } from "../event/event.model";
-import { EventAttendee } from "../event/eventAttendee.model";
-import { EventCohost } from "../event/eventCohost.model";
 
 @Table({ tableName: "user_identities", timestamps: true })
 export class UserIdentity extends Model {
@@ -28,13 +24,4 @@ export class UserIdentity extends Model {
 
   @HasOne(() => UserProfile)
   profile!: UserProfile;
-
-  @HasMany(() => Event, "hostId")
-  hostedEvents!: Event[];
-
-  @HasMany(() => EventAttendee, "userId")
-  eventAttendees!: EventAttendee[];
-
-  @HasMany(() => EventCohost, "userId")
-  eventCohosts!: EventCohost[];
 }
